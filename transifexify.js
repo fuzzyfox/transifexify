f072493d74bb5bb95d6d653963cbcf2e62ec5053
William Duyck <fuzzyfox0@gmail.com>
2014-02-19 09:02:47 +0000

    tidy + comment

--- transifexify.js
+++ transifexify.js
@@ -1,5 +1,6 @@
-// wicked closure pattern w/ hat tip to Paul Irish [source](https://gist.github.com/paulirish/315916)
 (function(window, document, undefined){
+
+	// finds all text nodes in a given element and its children
 	var textNodesUnder = function(element){
 		var node,
 			all  = [],
@@ -15,23 +16,35 @@
 		return all;
 	};
 
+	// get the selector for the element we want to prep for localization
 	var selector = window.prompt('Enter a selector to Transifexify');
+
+	// remove all whitespace from element to tidy results from textNodesUnder
+	// of all the whitespace in source.
 	document.querySelector(selector).innerHTML = document.querySelector(selector).innerHTML.replace(/\s+/g, ' ').trim();
 
 	var nodes = [],
 		transifexJSON = {},
-		transifexHTML = '';
+		transifexHTML = '<html>';
+
+	// get all text nodes for defined element
 	nodes = textNodesUnder(document.querySelector(selector));
 
+	// go through each text node we found and give it a sensible name, add 
+	// the contents of that node to a JSON file as {name:node.nodeValue}, and 
+	// swap the node value in the DOM for the name we gave it
 	nodes.forEach(function(node, idx){
 		transifexJSON['node' + idx + '-' + node.parentNode.localName] = node.nodeValue;
 		node.nodeValue = '{{ node' + idx + '-' + node.parentNode.localName + ' }}';
 	});
 
-	console.log(transifexJSON);
+	// show transifex json file to user for saving
 	window.showModalDialog('data:application/json;' + (window.btoa?'base64,'+btoa(JSON.stringify(transifexJSON)):JSON.stringify(transifexJSON)), null, 'center:1;dialogwidth:600;dialogheight:600');
 
-	transifexHTML = '<html>' + document.documentElement.innerHTML.replace(/<script src="https?:\/\/(.*?)\/transifexify\.js"><\/script>/, '') + '</html>';
+	// strip out transifexify tag from document
+	transifexHTML += document.documentElement.innerHTML.replace(/<script src="https?:\/\/(.*?)\/transifexify\.js"><\/script>/, '');
+	transifexHTML += '</html>';
 
+	// show modified HTML document source to user for saving
 	window.showModalDialog('data:text/plain;' + (window.btoa?'base64,'+btoa(transifexHTML):transifexHTML), null, 'center:1;dialogwidth:600;dialogheight:600');
 })(this, this.document);
