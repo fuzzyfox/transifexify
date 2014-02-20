c17724341bcc871a8292623930339c5ac3082ae5
William Duyck <fuzzyfox0@gmail.com>
2014-02-20 09:21:46 +0000

    remove need to CORS headers

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,37 +1,21 @@
 (function(window, document, undefined){
 	if(!window.transifexifyAdded){
-		// make request for transifexify sidebar
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://localhost:8000/transifexify.html');
-		xhr.onload = function(e){
-			if(xhr.readyState === 4 && xhr.status === 200){
-				// we can go full steam... sidebar will load
-				// add our css file to doc
-				var link = document.createElement('link');
-				link.rel = 'stylesheet';
-				link.href = 'http://localhost:8000/transifexify.css';
-				document.head.innerHTML = link.outerHTML + document.head.innerHTML;
+		// add our css file to the document
+		var link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.href = 'http://localhost:8000/transifexify.css';
+		document.head.innerHTML = link.outerHTML + document.head.innerHTML;
 
-				// add our wrapping code to body
-				document.body.innerHTML = '<div id="transifexifyDocument">'+ document.body.innerHTML +'</div>';
+		// add our wrapping code to body
+		document.body.innerHTML = '<div id="transifexifyDocument">'+ document.body.innerHTML +'</div>';
 
-				// inject sidebar
-				document.body.innerHTML += xhr.responseText;
+		// inject sidebar
+		document.body.innerHTML += '<div id="transifexify"><nav class="navbar navbar-inverse" role="navigation"><div class="navbar-header"><span class="navbar-brand">Transifexify</span></div><div style="height: 0px;padding: 0 15px 0 15px"><ul class="nav navbar-nav navbar-right"><li style="margin:0;"><a href="https://github.com/fuzzyfox/transifexify"><span class="fa fa-github"></span> fuzzyfox/transifexify</a></li></ul></div></nav><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, cupiditate.</p><form action="#" class="form clearfix" id="transifexifyForm"></form></div>';
 
-				// add transifexify sidebar js
-				var script = document.createElement('script');
-				script.src = 'http://localhost:8000/transifexify-sidebar.js';
-				document.body.appendChild(script);
-			}
-			else {
-				console.log('Failed to load sidebar: ', xhr, e);
-				var script = document.createElement('script');
-				script.src = 'http://localhost:8000/transifexify.js';
-				document.body.appendChild('script');
-			}
-
-			window.transifexifyAdded = true;
-		};
-		xhr.send();
+		// add transifexify sidebar js
+		var script = document.createElement('script');
+		script.src = 'http://localhost:8000/transifexify-sidebar.js';
+		document.body.appendChild(script);
+		window.transifexifyAdded = true;
 	}
 })(window, window.document);
