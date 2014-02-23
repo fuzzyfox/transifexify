9f31c227594690d1a43a63629a823a938f77c335
William Duyck <fuzzyfox0@gmail.com>
2014-02-23 13:10:40 +0000

    sensible inclusion of vendor code

--- tools/build-bookmarklet.js
+++ tools/build-bookmarklet.js
@@ -1,5 +1,5 @@
 var fs          = require('fs'),
-	parts       = ['./src/transifexify.js', './src/transifexify-sidebar.js'],
+	parts       = ['vendor/dom-TreeWalker-polyfill/src/TreeWalker-polyfill.js', './src/transifexify.js', './src/transifexify-sidebar.js'],
 	bookmarklet = '';
 
 parts.forEach(function(e, i, a){
